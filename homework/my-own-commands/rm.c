d697c9a7b447c545ab9ff18e3a797190d7d3f9d2
Abdul Alzaid <aalzaid1@lion.lmu.edu>
2014-02-19 01:12:35 -0800

    creating the remove system call, and also adding some checks to the rename system call

--- homework/my-own-commands/rm.c
+++ homework/my-own-commands/rm.c
@@ -10,15 +10,13 @@ int main(int argc, char *argv[]) {
     // We demonstrate the use of command-line arguments here.
     // But note the non-existent error handling (all the better
     // to illustrate the error code below).
-    
-    int result = syscall(38, argv[1], argv[2]);
+    int result = syscall(39, argv[1], 0777 /* Note: octal */);
     
     // A result of -1 means that something went wrong.  Otherwise,
     // check for your new directory!
     if (result == -1) {
         // Don't use this error message in "real" programs. O_o
-        char *errorMessage = "This renaming is wrong\n";
+        char *errorMessage = "Herp derp mkderp\n";
         syscall(4, 2, errorMessage, strlen(errorMessage));
     }
-
 }
