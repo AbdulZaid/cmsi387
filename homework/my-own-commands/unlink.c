432d12f2b81c7ec45e9238e230fc5d19ef3c28ac
Abdul Alzaid <mr.d7oom@gmail.com>
2014-03-22 01:26:57 -0700

    Update unlink.c

--- homework/my-own-commands/unlink.c
+++ homework/my-own-commands/unlink.c
@@ -1,27 +1,26 @@
-/*
- ** Abdul Alzaid.
- ** CMSI387: Operating System
- ** unlink.c
- **
- ** Description: This program implements the
- ** "unlink" system call (10) using the syscall function.
- **
- ** Contributions made by Jonathan Piatos, Carlos Agudo, and Abdul Alzaid.
- **
- */
-
+/* 
+**
+** CMSI387: Operating System
+** unlink.c
+**
+** Description: This program implements the 
+** "unlink" system call (10) using the syscall function.
+**
+** Contributions made by Abdul Alzaid, Carlos Agudo, and Jonathan Piatos
+**
+*/
 
 #include <stdio.h>
 #include <sys/file.h>
-#include <unistd.h>
+#include <unistd.h> 
 #include <string.h>
 #define ERR   (-1)
 #define SUCCESS (0)
 
 int main(int argc, char *argv[]){
-    
-    int result = syscall(10, argv[1], R_OK);
-    
+
+    int result = syscall(10, argv[1]);
+
     if (result == 0) {
         //message used simply for testing the syscall
         char *successMessage = "File deleted\n";
