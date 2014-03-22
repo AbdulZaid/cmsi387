c00bb804c3e659d1484835c5f090f961f9161495
Abdul Alzaid <mr.d7oom@gmail.com>
2014-03-22 01:24:46 -0700

    Update rename.c

--- homework/my-own-commands/rename.c
+++ homework/my-own-commands/rename.c
@@ -1,22 +1,21 @@
 /*
- ** Abdul Alzaid.
- ** CMSI387: Operating System
- ** rename.c
- **
- ** Description: This program demonstrates invocation of the rename
- ** system call (38) using the syscall function.
- **
- ** Contributions made by Abdul Alzaid, Carlos Agudo, and Jonathan Piatos
- **
- */
+** Abdul Alzaid.
+** CMSI387: Operating System
+** rename.c
+**
+** Description: This program demonstrates invocation of the rename
+** system call (38) using the syscall function.
+**
+** Contributions made by Abdul Alzaid, Carlos Agudo, and Jonathan Piatos
+**
+*/
 
 #include <linux/types.h>
 #include <linux/unistd.h>
 #include <string.h>
 
 int main(int argc, char *argv[]) {
-    
-    
+
     int result = syscall(38, argv[1], argv[2]);
     
     //messages are used to see if syscall worked or not
@@ -27,5 +26,5 @@ int main(int argc, char *argv[]) {
         char *completionMessage = "This renaming is right\n";
         syscall(4, 2, completionMessage, strlen(completionMessage));
     }
-    
+
 }
