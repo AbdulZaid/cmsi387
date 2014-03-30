ffce23c725b50fc386d6f9cd1ef5ef4674e9c4cb
AbdulZaid <aalzaid1@lion.lmu.edu>
2014-03-29 23:20:41 -0700

    working on the fork-exec and adding a while loop as Dondi said in the Video

--- homework/myshell/fork-exec/fork-exec.c
+++ homework/myshell/fork-exec/fork-exec.c
@@ -1,6 +1,7 @@
 #include <sys/types.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 
 /**
  * This program demonstrates the use of the fork() and exec()
@@ -9,28 +10,32 @@
 int main() {
     /* String to hold the command to run. */
     char command[256];
-    printf("Enter the command to run: ");
-    scanf("%s", command);
+    char *args[256];
+    int i;
+    do {
+    else {
+            args[i] = NULL;
+            /* Variable that will store the fork result. */
+            pid_t pid;
+            /* Perform the actual fork. */
+            pid = fork();
+            if (pid < 0) {
+                /* Error condition. */
+                fprintf(stderr, "Fork failed\n");
+                return -1;
 
-    /* Variable that will store the fork result. */
-    pid_t pid;
+            } else if (pid == 0) {
+                /* Child process. */
+                printf("Running...\n");
+                execvp(args[0], args);
 
-    /* Perform the actual fork. */
-    pid = fork();
-    if (pid < 0) {
-        /* Error condition. */
-        fprintf(stderr, "Fork failed\n");
-        return -1;
-    } else if (pid == 0) {
-        /* Child process. */
-        printf("Running...\n");
-        execlp(command, command, NULL);
-    } else {
-        /* Parent process. */
-        int result;
-        wait(&result);
-        printf("All done; result = %d\n", result);
-    }
-
+            } else {
+                /* Parent process. */
+                int result;
+                }
+                printf("All done; result = %d\n", result);
+            }
+        }
+    } while (!feof(stdin));
     return 0;
 }
