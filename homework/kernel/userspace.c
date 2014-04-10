a99aeaa9f156c10813ac62201c105691916bff43
Abdul Alzaid <mr.d7oom@gmail.com>
2014-04-10 12:41:10 -0700

    Update userspace.c

--- homework/kernel/userspace.c
+++ homework/kernel/userspace.c
@@ -1,6 +1,6 @@
 #include <unistd.h>
 #include <stdio.h>
 int main() {
-	syscall(351, "here the new syscall I struggled creating.");
+	syscall(351, "Yes I did it. Feels good after struggling.");
 	return 0;
 }
