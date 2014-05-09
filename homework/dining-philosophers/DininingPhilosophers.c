b9e0738f4ef42bccf42d8e48595f2b20c7c15fd8
AbdulZaid <aalzaid1@lion.lmu.edu>
2014-05-09 01:51:54 -0700

    Fixed all the inline comments that are made by Dondi. TABS are GONE

--- homework/dining-philosophers/DininingPhilosophers.c
+++ homework/dining-philosophers/DininingPhilosophers.c
@@ -1,6 +1,5 @@
 #include <pthread.h>
 #include <semaphore.h>
-
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
@@ -31,110 +30,96 @@ int randomWait(int bound) {
  *  This lets the philospher get a chopstick.
 */ 
 void useChopstick (int chopstick) {
-    // JD: TAAAAAAB below this line!!!
-	pthread_mutex_lock(&chopsticks[chopstick]);
-    // JD: Your error-checking code would go here, right before you alter the state of
-    //     the chopstick...
-	chopstick_state[chopstick] += 1;
+    pthread_mutex_lock(&chopsticks[chopstick]);
+    chopstick_state[chopstick] += 1;
 }
 
 /**
  *  This lets the philospher put down a chopstick.
 */ 
 void putDownChopstick (int chopstick) {
-    // JD: ...and here also, right before you alter the state as well.
-	pthread_mutex_unlock(&chopsticks[chopstick]);
-	chopstick_state[chopstick] -= 1;
-    // JD: ^^^Plus, you should alter the state *before* you unlock---if you don't,
-    //     another thread can now interfere.  Always do your alterations inside
-    //     the critical section.
+    pthread_mutex_unlock(&chopsticks[chopstick]);
+    chopstick_state[chopstick] -= 1;
 }
 
 /**
  * This function lets a philosopher pick up some chopsticks.
  */
 void eat (int philosopher) {
-	useChopstick(philosopher);
-	useChopstick((philosopher + 1) % 5);
-	philosopher_state[philosopher] = EATING;
-	randomWait(5);
+    useChopstick(philosopher);
+    useChopstick((philosopher + 1) % 5);
+    philosopher_state[philosopher] = EATING;
+    randomWait(5);
 }
 
 /**
  * This function lets a philosopher put down his chopsticks.
  */
 void finishEating (int philosopher) {
-	putDownChopstick(philosopher);
-	putDownChopstick((philosopher + 1) % 5);
-	philosopher_state[philosopher] = THINKING;
+    putDownChopstick(philosopher);
+    putDownChopstick((philosopher + 1) % 5);
+    philosopher_state[philosopher] = THINKING;
 }
 
 /**
  * This lets the philospher think.
  */
 void think (int philosopher) {
-	randomWait(5);
-	philosopher_state[philosopher] = HUNGRY;
+    randomWait(5);
+    philosopher_state[philosopher] = HUNGRY;
 }
-
-// JD: You're calling printPhilosophers before you define it---you need a
-//     forward declaration.
-void printPhilosophers();
 
 /**
  *  This function will let the philosophers eat and think.
  *  This method was implemented by looking at: http://rosettacode.org/wiki/Dining_philosophers
  */ 
 void* philosophize (void* philosopher) {
-	int id = *(int*) philosopher;
-	printPhilosophers(); // JD: You are calling this in multiple threads, and therefore there
-                         //     is a chance that the printf's will interleave.  To keep your
-                         //     output clean, you will need to synchronize on this (i.e., the
-                         //     display is itself a shared resource).
-	while (1) {
-		printPhilosophers();
-		if (philosopher_state[id] == THINKING) {
-			think(id);
-		} else if (philosopher_state[id] == HUNGRY) {
-			eat(id);
-		} else if (philosopher_state[id] == EATING) {
-			finishEating(id);
-		}
-	}
+    int id = *(int*) philosopher;
+    printPhilosophers(); 
+    while (1) {
+        printPhilosophers();
+        if (philosopher_state[id] == THINKING) {
+            think(id);
+        } else if (philosopher_state[id] == HUNGRY) {
+            eat(id);
+        } else if (philosopher_state[id] == EATING) {
+            finishEating(id);
+        }
+    }
 }
 
 /**
  * This function lets us see a visual representation of our philosophers.
  */
 void printPhilosophers () {
-	int i;
-	for (i = 0; i < 5; i++) {
-		if (philosopher_state[i] == THINKING) {
-			printf("  0_0  ");
-		} else if (philosopher_state[i] == HUNGRY) {
-			printf("  -.-  ");
-		} else if (philosopher_state[i] == EATING) {
-			printf(" \\^0^/ ");
-		}
-	}
-	printf("\n");
+    int i;
+    for (i = 0; i < 5; i++) {
+        if (philosopher_state[i] == THINKING) {
+            printf("  0_0  ");
+        } else if (philosopher_state[i] == HUNGRY) {
+            printf("  -.-  ");
+        } else if (philosopher_state[i] == EATING) {
+            printf(" \\^0^/ ");
+        }
+    }
+    printf("\n");
 }
 
 int main () {
-	int i;
-	pthread_t philosphers[5];
+    int i;
+    pthread_t philosphers[5];
 
-	for (i = 0; i < 5; i++) {
-		philosopher_state[i] = THINKING;
-		philosopher_number[i] = i;
-		chopstick_state[i] = 0;
-		pthread_mutex_init(&chopsticks[i], NULL);
-		pthread_create(&philosphers[i], NULL, philosophize, &philosopher_number[i]);
-	}
+    for (i = 0; i < 5; i++) {
+        philosopher_state[i] = THINKING;
+        philosopher_number[i] = i;
+        chopstick_state[i] = 0;
+        pthread_mutex_init(&chopsticks[i], NULL);
+        pthread_create(&philosphers[i], NULL, philosophize, &philosopher_number[i]);
+    }
 
-	/* join the threads. */
-	for (i = 0; i < 5; i++) {
-		pthread_join(philosphers[i], NULL);
-	}
-	return 0;
+    /* join the threads. */
+    for (i = 0; i < 5; i++) {
+        pthread_join(philosphers[i], NULL);
+    }
+    return 0;
 }
--- homework/dining-philosophers/addressTranslation/addressTranslation.c
+++ homework/dining-philosophers/addressTranslation/addressTranslation.c
@@ -2,7 +2,6 @@
  * Implementation of the software paging unit.
 */
 #include "addressTranslation.h"
-
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -15,22 +14,18 @@ void setPageTable(pagetable *pt) {
 * logical-to-physical paged memory address translation.
 */
 int getPhysical(int logical) {
-    // JD: There are TAAAAAABBBBBS below this line...NOOOOOOOOO.........
-	/**
-	 * The left most 4 bits and the right most four bits.
-	*/
-	int leftMostBits = ((logical & PAGEMASK) >> PAGEBITS);
-    // JD: ^^^Note, bitwise-"&" is not necessary due to right shift.
-	int rightMostBits = (logical & PAGESIZE);
-
-    // JD: The maximum value check can be unhardcoded by using the #define's
-    //     in the .h file.
-	if (logical < 0 || logical >= 256) {
+    /**
+    * The left most 4 bits and the right most four bits.
+    */
+    int leftMostBits = ((logical & PAGEMASK) >> PAGEBITS);
+    int rightMostBits = (logical & PAGESIZE);
+    
+    if (logical < 0 || logical >= 256) {
         return ERR_OUT_OF_RANGE;
     }
 
-	if (ptr[leftMostBits].valid == 0) {
-		return ERR_INVALID;
-	}
+    if (ptr[leftMostBits].valid == 0) {
+        return ERR_INVALID;
+    }
     return (((ptr[leftMostBits].frame) << PAGEBITS) + rightMostBits);
 }
